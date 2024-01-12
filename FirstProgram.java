548aaaa9a8b64102f04f04029b90cf70a6ba1b8e
MrMad2002 <155245899+MrMad2002@users.noreply.github.com>
2024-01-12 19:41:16 +0530

    Delete FirstProgram.java

--- FirstProgram.java
+++
@@ -1,5 +0,0 @@
-How to remove file from pushed git commit?
-Why some branches are main and some are master?
-How to remove file from github?
-Are premitive datatype only contain number?
-why we need float if we had double?
